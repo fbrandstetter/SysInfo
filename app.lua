1ad132e805af9bd4eaa5e5befbc8bcdd005486ce
Florian <info@fbrandstetter.eu.org>
2017-01-11 10:57:02 +0100

    Update app.lua

--- app.lua
+++ app.lua
@@ -1,5 +1,8 @@
 -- Grab specific data from server commands
 local ARCH = io.popen("uname -m"):read("*a")
 local KERNEL = io.popen("uname -mrs"):read("*a");
+local CPU = io.popen("cat /proc/cpuinfo"):read("*a");
+
 print(ARCH)
 print(KERNEL)
+print(CPU)
